0878518aeadef88c7c9015847e1ed7066553ab1b
radiantly <buildingsomethingfun@gmail.com>
2021-10-12 00:41:31 +0530

    Basic functionality complete!

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# simplest-sort
-The simplest quadratic time sort on the planet
+# Simplest sort
+
+Showcases the simplest sorting algorithm that works in quadratic time. From [here](https://arxiv.org/pdf/2110.01111.pdf).
+
+The pseudocode for this algo can be seen below (sorts an array with n elements in non decreasing order):
+
+```
+for i from 0 to n:
+    for j from 0 to n:
+        if Ar[i] < Ar[j]:
+            swap(Ar[i], Ar[j])
+```
+
+This repository houses a go program that uses exactly this algo, but animates how it works on your terminal! Demo below:
+
+![Screencast](demo/screencast.gif)
+
+## Usage
+
+Assuming you have `git` and `go` installed, you can simply clone and run.
+
+```sh
+git clone https://github.com/radiantly/simplest-sort
+
+go run main.go
+```
+
+Should work on any modern Linux terminal. Untested on MacOS and Windows.
+
+## License
+
+MIT
--- main.go
+++ main.go
@@ -2,27 +2,94 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"time"
 )
+
+func colorText(text string, color string) string {
+	switch color {
+	case "red":
+		return "\033[31m" + text + "\033[39m\033[49m"
+	case "green":
+		return "\033[32m" + text + "\033[39m\033[49m"
+	case "yellow":
+		return "\033[33m" + text + "\033[39m\033[49m"
+	default:
+		return text
+	}
+}
 
 func main() {
 	var n int
-	fmt.Print("Enter the number of elements you'd like to sort: ")
+	fmt.Print(colorText("Enter the number of elements you'd like to sort: ", "yellow"))
 	fmt.Scan(&n)
 
 	Ar := make([]int, n)
+	positions := make([]int, n)
 
-	fmt.Println("Enter the elements: ")
-	for i := range Ar {
-		fmt.Scan(&Ar[i])
-	}
+	recomputePositions := func() {
+		for i := range Ar {
+			positions[i] = int(math.Log10(float64(Ar[i]))) + 1
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if Ar[i] < Ar[j] {
-				Ar[i], Ar[j] = Ar[j], Ar[i]
+			if i > 0 {
+				positions[i] += positions[i-1] + 1
 			}
 		}
 	}
 
+	printCurrent := func(i, j int, color string) {
+		// Line 1
+		iSpaces := strings.Repeat(" ", positions[i]-1)
+		fmt.Println("\r\033[K"+colorText("i ", "yellow"), iSpaces, "▼")
+
+		// Line 2
+		fmt.Print("\r   [")
+		for k := range Ar {
+			num := strconv.Itoa(Ar[k])
+			if i == k || j == k {
+				fmt.Print(colorText(num, color))
+			} else {
+				fmt.Print(num)
+			}
+			if k != n-1 {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("]")
+
+		// Line 3
+		jSpaces := strings.Repeat(" ", positions[j]-1)
+		fmt.Println("\r\033[K"+colorText(" j", "yellow"), jSpaces, "▲\033[3A")
+	}
+
+	fmt.Print(colorText("Enter elements: ", "yellow"))
+	for i := range Ar {
+		fmt.Scan(&Ar[i])
+	}
+
+	fmt.Println()
+	recomputePositions()
+	for i := 0; i < n; i++ {
+		fmt.Print("\033[<N>A")
+		for j := 0; j < n; j++ {
+			if Ar[i] < Ar[j] {
+				printCurrent(i, j, "green")
+				time.Sleep(time.Second)
+
+				Ar[i], Ar[j] = Ar[j], Ar[i]
+				recomputePositions()
+
+				printCurrent(i, j, "green")
+			} else {
+				printCurrent(i, j, "red")
+			}
+			time.Sleep(700 * time.Millisecond)
+		}
+	}
+
+	fmt.Print("\r\033[K\r")
 	fmt.Println(Ar)
+	fmt.Println("\r\033[K\r" + colorText("Sorted! 🎉", "green"))
 }
